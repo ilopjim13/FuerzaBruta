ac615435e733d9bc7b365b6e634b3b45eb0975ce
ilopjim1306 <ljivan@iesrafaelalberti.es>
2025-01-28 20:13:15 +0100

    Initial commit

---
+++ FuerzaBruta/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace FuerzaBruta
+{
+    class Class1
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Open the text file using a stream reader.
+                using StreamReader reader = new("C:\\Users\\UsuarioT\\RiderProjects\\FuerzaBruta\\FuerzaBruta\\password.txt");
+
+                // Read the stream as a string.
+                string text = reader.ReadToEnd();
+
+                // Write the text to the console.
+                Console.WriteLine(text);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+    }
+}
