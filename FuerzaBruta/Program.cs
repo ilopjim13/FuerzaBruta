e6754a80c8c741b969e656a4aa36cd6b96d3fd43
ivanjim13 <ivangorrion57@gmail.com>
2025-01-29 04:14:35 +0100

    ahora funciona con multihilos (solo con 2)

--- FuerzaBruta/Program.cs
+++ FuerzaBruta/Program.cs
@@ -7,37 +7,36 @@ namespace FuerzaBruta
     class FuerzaBruta
     {
         static string[] _words = [];
-        static bool _found = false;
+        static volatile bool _found;
         [STAThread]
         static void Main(string[] args)
         {
             
             try
             {
-                using StreamReader reader = new("password.txt");
-
-                string diccionario = reader.ReadToEnd();
-                
-                _words = diccionario.Split('\n');
+                LoadDiccionario();
 
                 string password = GetPasswordRandom();
                 
                 Thread thread = new Thread(() => GetPassword(password));
+                Thread thread2 = new Thread(() => GetPassword2(password));
                 
                 thread.Start();
+                thread2.Start();
                 
                 thread.Join();
+                thread2.Join();
 
-                Console.WriteLine($"Palabra aleatoria: {password}");
+                Console.WriteLine($"Password: {password}");
             }
             catch (IOException e)
             {
-                Console.WriteLine("The file could not be read:");
+                Console.WriteLine("No se ha podido leer el archivo");
                 Console.WriteLine(e.Message);
             }
             finally
             {
-                Console.WriteLine("Executing finally block.");
+                Console.WriteLine("Terminado");
             }
         }
 
@@ -45,13 +44,35 @@ namespace FuerzaBruta
         {
             try
             {
-                using StreamReader reader = new("password.txt");
                 foreach (var word in _words)
                 {
+                    if (_found) break;
                     if (word == password)
                     {
                         _found = true;
-                        Console.WriteLine($"Password found: {word}");
+                        Console.WriteLine($"Password found for 1: {word}");
+                        break;
+                    } 
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+        
+        static void GetPassword2(string password)
+        {
+            try
+            {
+                foreach (var word in _words.Reverse())
+                {
+                    if (_found) break;
+                    if (word == password)
+                    {
+                        _found = true;
+                        Console.WriteLine($"Password found for 2: {word}");
                         break;
                     }
                 }
@@ -67,6 +88,13 @@ namespace FuerzaBruta
         {
             Random rnd = new();
             return _words[rnd.Next(_words.Length)];
+        }
+
+        static void LoadDiccionario()
+        {
+            using StreamReader reader = new("password.txt");
+            string diccionario = reader.ReadToEnd();
+            _words = diccionario.Split('\n');
         }
     }
 }
